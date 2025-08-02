f66491929e49feca4ac1c1f07f88dfe747c75e0a
shitesh372 <shiteshkumarsharma@gmail.com>
2025-08-02 12:17:04 +0530

    Initial commit

---
+++ simple.py
@@ -0,0 +1,10 @@
+num1=float(input("Enter first number:"))
+num2=float(input("Enter second number:"))
+addition=num1+num2
+subtraction=num1-num2
+multiply=num1*num2
+division=num1/num2
+print("addition:", addition)
+print("subtraction:", subtraction)
+print("multiplication:", multiply)
+print("division:", division)
